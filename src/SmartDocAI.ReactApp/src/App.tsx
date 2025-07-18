801e9d2c81dca6e71247e9a61ddb04d77ae4fd1c
codedm24 <mithun.adroit@gmail.com>
2025-07-18 17:50:16 +0530

    Add SmartDocAI.ReactApp (React+Vite+Axios+MaterialUI)

---
+++ src/SmartDocAI.ReactApp/src/App.tsx
@@ -0,0 +1,28 @@
+//
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+import Home from "./pages/Home";
+import Upload from "./pages/Upload";
+
+function App() {
+  return (
+    <Router>
+      <nav style={{ display: "flex", gap: "1rem", padding: "1rem" }}>
+        <Link to="/">Home</Link>
+        <Link to="/upload">Upload Document</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/upload" element={<Upload />} />
+      </Routes>
+    </Router>
+    // <Routes>
+    //   <Route path="/" element={<MainLayout />}>
+    //     <Route index element={<Home />} />
+    //     <Route path="upload" element={<Upload />} />
+    //   </Route>
+    // </Routes>
+  );
+}
+
+export default App;
---
+++ src/SmartDocAI.ReactApp/src/main.tsx
@@ -0,0 +1,35 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+// import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: { main: "#1976d2" },
+    secondary: { main: "#9c27b0" },
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <HelmetProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </HelmetProvider>
+    {/* <BrowserRouter>
+    <HelmentProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+      </HelmentProvider>
+    </BrowserRouter> */}
+  </StrictMode>
+);
