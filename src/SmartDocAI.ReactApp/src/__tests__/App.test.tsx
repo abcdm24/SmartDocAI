44d11d37c34844fda2a9084cda811b8276ff960e
codedm24 <mithun.adroit@gmail.com>
2025-07-22 12:39:35 +0530

    Added frontenf tests with Vitest and fixed CI test flow

---
+++ src/SmartDocAI.ReactApp/src/__tests__/App.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+import { describe, it, expect } from "vitest";
+import { HelmetProvider } from "react-helmet-async";
+
+describe("App Component", () => {
+  it("renders without crashing", () => {
+    render(
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    );
+    expect(screen.getByText("SmartDoc AI")).toBeInTheDocument();
+  });
+});
