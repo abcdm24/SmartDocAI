8f633385878d2fe706f8820ae08c5011a5e036fa
codedm24 <mithun.adroit@gmail.com>
2025-07-27 17:55:52 +0530

    Update code for data persistence

--- src/SmartDocAI.Application/DTOs/DocumentDto.cs
+++ src/SmartDocAI.Application/DTOs/DocumentDto.cs
@@ -1,4 +1,7 @@
-﻿using SmartDocAI.Domain.Enums;
+﻿using Document = SmartDocAI.Domain.Entities.Document;
+using SmartDocAI.Domain.Enums;
+using SmartDocAI.Domain.Interfaces;
+using SmartDocAI.Domain.ValueObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +10,34 @@ using System.Threading.Tasks;
 
 namespace SmartDocAI.Application.DTOs
 {
-    public class DocumentDto
+    public class DocumentDto : IDocument
     {
         public Guid Id { get; set; }
-        public string Name { get; set; } = string.Empty;
+        public string FileName { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DocumentType FileType { get; set; }
+        public string StoragePath { get; set; } = string.Empty;
         public DateTime UploadedAt { get; set; } = DateTime.UtcNow;
+        public DocumentMetadata Metadata { get; set; } = new();
 
+        /// <summary>
+        /// default constructor
+        /// </summary>
+        public DocumentDto() { 
+        }
+
+        /// <summary>
+        /// constructor that takes Document object
+        /// </summary>
+        /// <param name="doc"></param>
+        public DocumentDto(Document doc) { 
+            Id = doc.Id;
+            FileName = doc.FileName;
+            Content = doc.Content;
+            FileType = doc.FileType;
+            StoragePath = doc.StoragePath;
+            UploadedAt = doc.UploadedAt;
+            Metadata = doc.Metadata;
+        }
     }
 }
